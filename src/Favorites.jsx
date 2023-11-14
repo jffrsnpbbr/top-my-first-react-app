317b7902fdb0dd42b283babc49399c83c1e271d9
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-14 09:51:42 +0800

    React components assignment

---
+++ src/Favorites.jsx
@@ -0,0 +1,15 @@
+function FavoriteFoods() {
+  return (
+    <section>
+      <h2>Favorite Foods</h2>
+      <ul>
+        <li>Spicy Fried Chicken</li>
+        <li>Kalderetang Baka</li>
+        <li>Chopsuey</li>
+        <li>Ginisang Pechay na may Giniling na baboy</li>
+      </ul>
+    </section>
+  );
+}
+
+export { FavoriteFoods };
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
+import { FavoriteFoods } from './Favorites.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Greeting />
+    <FavoriteFoods />
   </React.StrictMode>,
 )
